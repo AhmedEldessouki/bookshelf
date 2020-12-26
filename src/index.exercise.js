ae63cc21220626471cb990e81f494aef26a46635
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-12-26 15:25:45 +0300

    extra 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,19 +3,53 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import {Logo} from './components/logo'
 
+import '@reach/dialog/styles.css'
+import Dialog from '@reach/dialog'
+import VisuallyHidden from '@reach/visually-hidden'
+
 function App() {
-  const [clicked, setClicked] = React.useState('')
+  const [show, setShow] = React.useState('none')
+
+  const open = value => setShow(value)
+  const close = () => setShow('none')
   return (
-    <>
-      <Logo />
-      <button type="button" onClick={() => setClicked('No `-`')}>
-        login
-      </button>
-      <button type="button" onClick={() => setClicked('Get Out Of Here! -_-')}>
-        register
-      </button>
-      <h1 style={{color: 'red'}}>{clicked}</h1>
-    </>
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <Logo height="100" width="100" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button type="button" onClick={() => open('login')}>
+          login
+        </button>
+        <button type="button" onClick={() => open('registration')}>
+          register
+        </button>
+      </div>
+      <Dialog
+        isOpen={show !== 'none'}
+        onDismiss={close}
+        aria-label="login or registration Form"
+      >
+        <button className="close-button" onClick={close}>
+          <VisuallyHidden>Close</VisuallyHidden>
+          <span aria-hidden>×</span>
+        </button>
+        <h1>{show}</h1>
+        <form
+          style={{display: 'flex'}}
+          onSubmit={e => {
+            e.preventDefault()
+            console.log(
+              e.currentTarget.username.value,
+              e.currentTarget.password.value,
+            )
+          }}
+        >
+          <input type="text" name="username" placeholder="Enter Username" />
+          <input name="password" type="password" placeholder="Enter Password" />
+          <button type="submit">Submit</button>
+        </form>
+      </Dialog>
+    </div>
   )
 }
 
